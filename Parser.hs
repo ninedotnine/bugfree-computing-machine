ed54f9bee5e8796715ac34e1c8446f0a82d100a8
dan soucy <soucydi194@potsdam.edu>
2015-07-07 17:37:40 -0400

    parser v12

--- Parser.hs
+++ Parser.hs
@@ -1,4 +1,4 @@
-module Parser11 (Instruction(..), Word(..), parseInstructions) where 
+module Parser12 (Instruction(..), Word(..), parseInstructions) where 
 
 import Text.ParserCombinators.Parsec hiding (try)
 -- import Text.Parsec.Prim hiding (runParser)
@@ -19,15 +19,21 @@ import Text.Read (readMaybe)
 
 import Instructions
 
--- testfile = "testfile5.sax"
-testfile = "programs/testfile5.sax"
+testfile = "programs/testfile5"
 
 main :: IO ()
 main = do
     putStrLn "%SXX+Object Module"
     putStrLn $ "# object module for file: TEST LOL"
     c <- readFile testfile
-    let result = runWriter $ runParserT parseInstructions 0 testfile c
+--     let result = runWriter $ runParserT parseInstructions 0 testfile c
+    let tempResult :: Writer 
+                        [(String, Integer)] 
+                        (Either ParseError (Integer, String, [Word]))
+        tempResult = runParserT parseInstructions (0, "") testfile c
+        result :: (Either ParseError (Integer, String, [Word]), [(String, Integer)])
+--         result = runWriter $ runParserT parseInstructions (0, "") testfile c
+        result = runWriter $ tempResult
     putStr "result is: " >> print result
     putStrLn  "------------------------------------------"
     case fst result of
@@ -39,13 +45,20 @@ main = do
 MyParser is a type 
 ParsecT is a monad transformer
 String is the stream type
-Integer is the state. i use it to count words, both so i can output the 
-    text length and so i can track where the labels are.
+(Integer, String) is the state. 
+    i use the Integer to count words, both so i can output the text length and 
+        so i can track where the labels are.
+    the String is the name of the entry point after it has been found. 
+        it is the empty string if no entry point is found.
 Writer [(String, Integer)] is the transformed monad. when a label is parsed,
     a (String, Integer) pair is appended using its monoid instance. the String
     in this case is the name of the label; the Integer is its location. 
 -}
-type MyParser a = ParsecT String Integer (Writer [(String, Integer)]) a
+type MyParser a = ParsecT 
+                    String 
+                    (Integer, String) 
+                    (Writer [(String, Integer)]) 
+                    a
 
 {-
 Word is an algebraic data type
@@ -64,13 +77,15 @@ data Word = Lit Integer
 
 --------------------------------- parser begins here
 
-parseInstructions :: MyParser (Integer, [Word])
+-- parseInstructions :: MyParser (Integer, [Word])
+parseInstructions :: MyParser (Integer, String, [Word])
 parseInstructions = do 
     res <- join <$> (many (try parseInstruction)) `sepBy` skipJunk
     skipMany skipJunk
     eof
-    count <- getState
-    return (count, res)
+    (count, entry) <- getState
+--     count <- getState
+    return (count, entry, res)
 
 parseInstruction :: MyParser Word
 parseInstruction = do
@@ -82,26 +97,30 @@ parseInstruction = do
         <|> try parseEntry 
         <|> try parseNewLabel 
         <|> parseWord
-    modifyState (+1) -- add one to the count of instructions
+--     modifyState (+1) -- add one to the count of instructions
+--     modifyState (updateLocationCounter (+1)) -- add one to the count of instructions
+    loc (+1) -- add one to the count of instructions
     return code
 
 parseDS :: MyParser Word
 parseDS = do
-    oneOf "Dd" >> oneOf "Ss" >> skipSpaces
+    caseInsensitiveString "ds" >> skipSpaces
     (Lit size) <- parseInt
-    modifyState (\x -> x-1) -- a DS should not increase the text length...
-    modifyState (+size) -- but its operand should, by its value
+--     a DS should not increase the text length...
+--     modifyState (+size) -- but its operand should, by its value
+--     modifyState (updateLocationCounter (+size)) -- but its operand should, by its value
+    loc (+size) -- but its operand should, by its value
     return (DS size)
 
 -- FIXME: still doesn't handle strings or other fancy things like expressions
 parseDW :: MyParser Word
 parseDW = do
-    oneOf "Dd" >> oneOf "Ww" >> skipSpaces
---     (Lit arg) <- parseInt <|> parseChar
+    caseInsensitiveString "dw" >> skipSpaces
     args <- map unLit <$> (parseInt <|> parseChar) `sepBy1` (char ',' >>spaces)
         -- a DW should increase the text length by the number of arguments
-    modifyState (\x -> x + (genericLength args) - 1) 
---     return (DW [arg])
+--     modifyState (\x -> x + (genericLength args) - 1) 
+--     modifyState (updateLocationCounter (\x -> x + (genericLength args) - 1))
+    loc (\x -> x + (genericLength args) - 1)
     return (DW args)
         where unLit (Lit x) = x
 
@@ -112,7 +131,11 @@ parseEntry = do
     header <- letter
     tailer <- many labelChar
     let name = (header:tailer)
-    modifyState (\x -> x-1) -- the entry should not increase the text length
+--     modifyState (\x -> x-1) -- the entry should not increase the text length
+--     modifyState (updateLocationCounter (\x -> x-1)) -- the entry should not increase the text length
+    loc (\x -> x-1) -- the entry should not increase the text length
+--     setEntry name
+    modifyState (\(i, s) -> (i, name))
     return (Entry name)
 
 -- FIXME: local labels are not implemented
@@ -123,9 +146,12 @@ parseNewLabel = do
     let name = (header:tailer)
     char ':' 
     skipMany skipJunk
-    pos <- getState -- get the current position in the count
+--     pos <- getState -- get the current position in the count
+    pos <- getLocationCounter -- get the current position in the count
     lift $ tell [(name, pos)] -- append it to the list of labels
-    modifyState (\x -> x-1) -- a label should not increase the text length
+--     modifyState (\x -> x-1) -- a label should not increase the text length
+--     modifyState (updateLocationCounter (\x -> x-1)) -- a label should not increase the text length
+    loc (\x -> x-1) -- a label should not increase the text length
     return (NewLabel name)
 
 parseWord :: MyParser Word
@@ -160,11 +186,6 @@ skipToEOL = anyChar `manyTill` newline >> skipMany space
 -- caseInsensitiveChar :: Char -> GenParser Char state Char
 caseInsensitiveChar :: Char -> MyParser Char
 caseInsensitiveChar = (\c -> (char (toLower c) <|> char (toUpper c)) >> pure c)
-{-
-caseInsensitiveChar c = do
-  char (toLower c) <|> char (toUpper c)
-  return c
--}
 
 -- caseInsensitiveString :: String -> GenParser Char state String
 caseInsensitiveString :: String -> MyParser String
@@ -178,3 +199,19 @@ silently truncated to 30 characters.
 -}
 labelChar :: MyParser Char
 labelChar = letter <|> digit <|> oneOf "._"
+
+updateLocationCounter :: (Integer -> Integer) -> (Integer, String) -> (Integer, String)
+updateLocationCounter f (i, s) = (f i, s)
+
+loc :: (Integer -> Integer) -> MyParser ()
+loc = modifyState . updateLocationCounter
+
+setEntry :: String -> MyParser ()
+setEntry = modifyState . setEntry'
+    where 
+        setEntry' str (i, "") = (i, str)
+        setEntry' _ _ = error "multiple entries?" 
+
+getLocationCounter = do
+    (i, s) <- getState
+    return i
