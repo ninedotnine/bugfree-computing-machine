c203f7867e11948e1a040a7113eaf8cc9f08d850
dan soucy <dev@danso.ca>
2015-07-14 02:46:41 -0400

    began using Parsec for hsxx
    currently it can
        - read the first line ("%SXX+E")
        - populate the vector with the text
        - (maybe) handle DWs

--- src/hsxx/Parser.hs
+++ src/hsxx/Parser.hs
@@ -1,4 +1,6 @@
 -- {-# LANGUAGE OverloadedStrings #-} 
+{-# LANGUAGE TypeSynonymInstances #-} 
+{-# LANGUAGE FlexibleInstances #-} 
 -- {-# OPTIONS_GHC -Wall #-} 
 -- {-# OPTIONS_GHC -fno-warn-unused-do-bind #-} 
 
@@ -16,11 +18,14 @@ import Text.ParserCombinators.Parsec hiding (try, label, labels, (<|>))
 import Text.Parsec.Prim hiding (runParser, label, labels, (<|>))
 -- import Text.Parsec.Prim hiding (try, runParser)
 -- import Text.Parsec.Prim (Parsec)
+import Data.Vector.Generic hiding ((++), Vector, mapM_)
+import qualified Data.Vector.Unboxed as IM
 import Data.Vector.Unboxed.Mutable (new, write, MVector)
 import qualified Data.Vector.Unboxed.Mutable as V
 -- import Text.Parsec.Prim
 -- import Control.Monad (join)
 import Control.Monad
+import Control.Monad.Trans (liftIO)
 import Control.Monad.Primitive (PrimState)
 -- import Control.Monad.Identity
 import Control.Monad.Writer (Writer, runWriter, tell, lift)
@@ -30,7 +35,7 @@ import Control.Applicative hiding (many)
 -- import Control.Applicative ((<$>))
 -- import Control.Applicative hiding ((<|>))
 import Data.Char (toUpper, toLower, ord)
-import Data.List (genericLength)
+import Data.List (genericLength, intersperse)
 import Data.String (IsString, fromString)
 import Data.Int
 -- import Data.Foldable (traverse_)
@@ -51,20 +56,29 @@ populateVector :: MyVector -> String -> IO ()
 populateVector mem input = do
 --     print "dumb"
 --     let result :: Either ParseError (Int16)
-    result <- runParserT fillVector 16 "input" input -- start filling instructions at 16
+    result <- runParserT (fillVector mem) 16 "input" input -- start filling instructions at 16
     case result of
         Left err -> putStrLn ("error: " ++ (show err)) >> exitFailure
-        Right r -> putStrLn "successful parse"
+        Right r -> putStrLn "successful parse" 
 --     print result
-    
+
 -- fillVector :: MyParser ()
-fillVector :: ParsecT String Int16 IO ()
-fillVector = do 
-    string "%SXX-E" >> skipToEOL
+fillVector :: MyVector -> ParsecT String Int IO ()
+fillVector mem = do 
+    header
+--     instruction mem `sepBy` skipSpaces
+    instruction mem `endBy` skipSpaces
+    eof
+    lastInstruction <- getState 
+    liftIO $ putStr "instruction space: " 
+    liftIO $ printVector 16 (lastInstruction-16) mem
 --     res <- join <$> many (try instruction) `sepBy` skipJunk
 --     skipMany skipJunk *> eof
 --     (counter, entry, _) <- getState
 --     return (counter, entry, res)
+
+header :: MyParser ()
+header = string "%SXX-E" >> skipToEOL
 
 traceM :: (Monad m) => String -> m ()
 traceM str = trace str $ return ()
@@ -85,6 +99,43 @@ main = undefined
         Right r -> print r
     putStrLn "okay"
     -}
+
+skipSpaces :: MyParser ()
+skipSpaces = many1 space >> return ()
+
+-- FIXME this needs testing
+sxxDW :: MyVector -> MyParser ()
+sxxDW mem = do 
+    char ':'
+    val <- readNum
+    index <- getState
+    liftIO $ putStrLn $ "dw: " ++ show val
+    liftIO $ write mem index val
+    modifyState (+ (fromIntegral val))
+
+instruction :: MyVector -> MyParser ()
+instruction mem = do 
+    val <- readNum
+    index <- getState
+    liftIO $ putStrLn $ "read: " ++ show val
+    liftIO $ write mem index val
+    modifyState (+1)
+    return ()
+-- fmap Lit intOrChar <* loc (+1)
+--     <|> try asmEQU 
+--     <|> try asmDS 
+--     <|> try asmDW 
+--     <|> try opcode <* loc (+1)
+--     <|> label <* loc (+1)
+
+readNum :: MyParser Int32
+-- readNum = read <$> many1 digit 
+readNum = do
+    str <- readMaybe <$> many1 digit  
+    case str of
+        Just x -> return x
+        Nothing -> fail "oopsie"
+    
 
 {-
 parseEverything :: SourceName 
@@ -107,10 +158,11 @@ parseEverything' = (runWriter .) . runParserT instructions (0, "", "")
 MyParser is a type 
 ParsecT is a monad transformer
 String is the stream type
-Int16 is the state, it is the current index of the vector
+Int is the state, it is the current index of the vector
 IO is the transformed monad.
 -}
-type MyParser a = ParsecT String Int16 IO a
+
+type MyParser a = ParsecT String Int IO a
 
 newtype EntryPoint = EntryPoint String deriving (Eq)
 
@@ -157,3 +209,14 @@ skipComment = spaces *> char '#' *> skipToEOL
 
 skipToEOL :: MyParser ()
 skipToEOL = anyChar `manyTill` newline *> skipMany space
+
+-----------------------------------------
+
+printVector :: Int -> Int -> MyVector -> IO ()
+printVector i n vec = do
+    let frozen :: IO (IM.Vector Int32)
+        frozen = freeze $ V.slice i n vec
+    lst <- toList <$> frozen
+    putChar '['
+    mapM_ putStr (intersperse ", " (show <$> lst))
+    putStrLn "]"
