9fe238eed6fc38eb8723a970c4d5b96fc51a3f1e
dan soucy <dev@danso.ca>
2015-07-23 16:42:30 -0400

    decommenter now removes leading spaces

--- src/common/Decommenter.hs
+++ src/common/Decommenter.hs
@@ -1,17 +1,21 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Decommenter (decomment) where
 
 -- FIXME : a bug when '#' appears in quotes
 
--- {-# LANGUAGE OverloadedStrings #-}
 
 -- import Control.Monad (liftM2)
 import Control.Monad 
 import Control.Applicative
--- import Data.Text (Text, null, lines, unlines, takeWhile, head)
-import qualified Data.Text as T (Text, null, lines, unlines, takeWhile, head)
-import Data.Text.IO (interact)
-import Prelude hiding (interact)
 
+import qualified Data.Text as T (Text, null, lines, takeWhile, dropWhile, head)
+import qualified Data.Text.IO as TextIO (interact)
+
+import Data.Monoid
+import Data.String (IsString)
+import Data.Char (isSpace)
+import Prelude hiding (unlines)
 {- 
 this module exports decomment, which removes:
     empty lines
@@ -19,12 +23,15 @@ this module exports decomment, which removes:
     anything in a line following a '#' 
  -}
 
--- main = interact decomment'
-main = interact decomment'
+-- main = interact decomment
+main = TextIO.interact decomment'
 
 decomment' :: T.Text -> T.Text
-decomment' = T.unlines . map (T.takeWhile (/= ('#'))) . removeLines . T.lines
-    where removeLines = filter $ not . ((||) . T.null <*> (=='#') . T.head)
+decomment' = unlines . map (T.dropWhile isSpace) . removeInlineComments 
+             . removeLines . T.lines
+    where 
+        removeLines = filter $ not . ((||) . T.null <*> (=='#') . T.head)
+        removeInlineComments = map (T.takeWhile (/= ('#'))) 
     {-
         removeLines :: [T.Text] -> [T.Text]
         removeLines = filter (not . badLine) where
@@ -34,7 +41,8 @@ decomment' = T.unlines . map (T.takeWhile (/= ('#'))) . removeLines . T.lines
 
 decomment :: String -> String
 -- decomment = unlines . map (takeWhile (/= ('#'))) . removeLines . lines where
-decomment = unlines . removeInlineComments . removeLines . lines where
+decomment = unlines . map (dropWhile isSpace) . removeInlineComments 
+            . removeLines . lines where
 --     removeLines = filter $ liftM2 (||) (not . null) ((/='#') . head)
 --     removeLines = filter (ap ((||) . ('#' /=) . head) (not . null))
 --     removeLines = filter (ap ((||) . not . null) (('#' /=) . head))
@@ -42,3 +50,8 @@ decomment = unlines . removeInlineComments . removeLines . lines where
     removeInlineComments = map $ takeWhile (/= ('#')) -- FIXME: quoted strings
 --         removeLines = filter (not . badLine) where 
 --             badLine :: String -> Bool
+
+-- this is just like the unlines of the Prelude, except no newline at the end
+unlines :: (IsString a, Monoid a) => [a] -> a
+unlines [] = ""
+unlines (x:xs) = mconcat $ x : fmap ("\n" <>) xs
