82d0875a83d4c01e1d515073f6359f43ac8f2ae5
dan soucy <soucydi194@potsdam.edu>
2015-07-25 03:32:41 -0400

    SXXParser.hs: deleted many comments, old stuff

--- src/SXXParser.hs
+++ src/SXXParser.hs
@@ -1,47 +1,21 @@
--- {-# LANGUAGE OverloadedStrings #-} 
-{-# LANGUAGE TypeSynonymInstances #-} 
-{-# LANGUAGE FlexibleInstances #-} 
 {-# LANGUAGE ImplicitParams #-}
--- {-# LANGUAGE NoMonomorphismRestriction #-} 
 {-# OPTIONS_GHC -Wall #-} 
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-} 
 
 module SXXParser (MyVector, populateVector, printVector) where
 
-
 import System.Exit (exitFailure)
 import Text.ParserCombinators.Parsec hiding (try, label, labels, (<|>))
--- import Text.Parsec.Prim hiding (runParser)
 import Text.Parsec.Prim hiding (runParser, label, labels, (<|>))
--- import Text.Parsec.Prim hiding (try, runParser)
--- import Text.Parsec.Prim (Parsec)
 import Data.Vector.Generic hiding ((++), Vector, mapM_)
-import qualified Data.Vector.Unboxed as IM
--- import Data.Vector.Unboxed.Mutable (write, MVector)
-import qualified Data.Vector.Unboxed.Mutable as V
--- import Text.Parsec.Prim
--- import Control.Monad (join)
-import Control.Monad
+import qualified Data.Vector.Unboxed as IM (Vector)
+import qualified Data.Vector.Unboxed.Mutable as V (read, slice)
+import Control.Monad (when)
 import Control.Monad.Trans (liftIO)
--- import Control.Monad.Primitive (PrimState)
--- import Control.Monad.Identity
--- import Control.Monad.Writer (Writer, runWriter, tell, lift)
--- import Control.Monad.State (runState, evalState, execState)
--- import Control.Applicative hiding (many, (<|>))
 import Control.Applicative hiding (many)
--- import Control.Applicative ((<$>))
--- import Control.Applicative hiding ((<|>))
--- import Data.Char (toUpper, toLower, ord)
--- import Data.List (genericLength, intersperse)
 import Data.List (intersperse)
--- import Data.String (IsString, fromString)
 import Data.Int (Int32)
--- import Data.Foldable (traverse_)
 import Text.Read (readMaybe)
-
--- import Data.Map.Strict (Map, (!))
--- import qualified Data.Map.Strict as Map (Map, singleton)
--- import Data.Map.Strict as Map (Map, singleton)
 
 -- import Debug.Trace (traceM)
 
@@ -56,9 +30,6 @@ IO is the transformed monad.
 -}
 
 type MyParser a = ParsecT String Int IO a
-
--- type MyVector = MVector (PrimState IO) Int32
-
 
 baseAddr :: Integral a => a
 baseAddr = 16 -- the base address must be bigger than 15
@@ -111,7 +82,6 @@ notDS mem = let ?mem = mem in do
     val <- readNum
     index <- getState
     liftIO $ putStrLn $ "read: " ++ show val
---     liftIO $ V.write mem index val
     liftIO $ write index val
     modifyState (+1)
 
@@ -150,37 +120,3 @@ printVector i n vec = do
     putChar '['
     mapM_ putStr (intersperse ", " (show <$> lst))
     putStrLn "]"
-
-{-
-    --------------------------------- old parser begins here
-
-    -- instructions :: MyParser (Integer, EntryPoint, [Token])
-instructions :: MyParser (Integer, EntryPoint, [Token])
-    instructions = do 
-    res <- join <$> many (try instruction) `sepBy` skipJunk
-    skipMany skipJunk *> eof
-    (counter, entry, _) <- getState
-return (counter, entry, res)
-
-    instruction :: MyParser Token
-    instruction = skipMany skipJunk *>
-fmap Lit intOrChar <* loc (+1)
-    <|> try asmEQU 
-    <|> try asmDS 
-    <|> try asmDW 
-    <|> try asmEntry 
-    <|> try asmExtern 
-    <|> try asmPublic
-    <|> try newLabel 
-    <|> try opcode <* loc (+1)
-    <|> label <* loc (+1)
-
-skipJunk :: MyParser ()
-skipJunk = skipSpaces <|> skipComment
--- skipJunk = spaces <|> skipComment
-
-skipSpaces :: MyParser ()
-skipSpaces = skipMany1 space <?> "" -- silence this; it's handled elsewhere
-
--}
-
