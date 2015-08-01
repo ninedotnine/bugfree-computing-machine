49db1e3b885c876482060327245aa9f1edbac4c9
dan soucy <dev@danso.ca>
2015-08-01 16:20:05 -0400

    Compiler: separate main functionality from parser
    modified:   CParser.hs
    new file:   Compiler.hs

---
+++ src/Compiler.hs
@@ -0,0 +1,16 @@
+
+import Text.Parsec.String (parseFromFile)
+
+import CParser hiding (main)
+
+testfile :: FilePath
+testfile = "../test/c/else.c"
+
+main :: IO ()
+main = do
+    putStrLn "HELLO"
+    eith <- parseFromFile (parseLex parseDefs) testfile
+    case eith of 
+        Left err -> print err
+        Right r -> print r
+    putStrLn "done"
